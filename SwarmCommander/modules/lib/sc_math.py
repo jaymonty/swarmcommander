40a75f962665c9500f84067b2a60037b348313ab
Michael Day <mday299@yahoo.com>
2014-12-01 15:50:58 -0800

    math: Some GPS helper functions broken into a new math module.

---
+++ SwarmCommander/modules/lib/sc_math.py
@@ -0,0 +1,48 @@
+"""
+    Swarm Commander Math Utilities
+
+    Michael Day
+    Nov 2014
+"""
+
+import math
+
+radius_of_earth = 6378100.0
+
+def gps_distance(lat1, lon1, lat2, lon2):
+    '''return distance between two points in meters,
+    coordinates are in degrees
+    thanks to http://www.movable-type.co.uk/scripts/latlong.html'''
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+    lon1 = math.radians(lon1)
+    lon2 = math.radians(lon2)
+    dLat = lat2 - lat1
+    dLon = lon2 - lon1
+
+    a = math.sin(0.5*dLat)**2 + math.sin(0.5*dLon)**2 * math.cos(lat1) * math.cos(lat2)
+    c = 2.0 * math.atan2(math.sqrt(a), math.sqrt(1.0-a))
+    return radius_of_earth * c
+
+def wrap_valid_longitude(lon):
+    ''' wrap a longitude value around to always have a value in the range
+        [-180, +180) i.e 0 => 0, 1 => 1, -1 => -1, 181 => -179, -181 => 179
+    '''
+    return (((lon + 180.0) % 360.0) - 180.0)
+
+def gps_newpos(lat, lon, bearing, distance):
+    '''extrapolate latitude/longitude given a heading and distance
+    thanks to http://www.movable-type.co.uk/scripts/latlong.html
+    '''
+    lat1 = math.radians(lat)
+    lon1 = math.radians(lon)
+    brng = math.radians(bearing)
+    dr = float(distance)/radius_of_earth
+
+    lat2 = math.asin(math.sin(lat1)*math.cos(dr) +
+                     math.cos(lat1)*math.sin(dr)*math.cos(brng))
+    lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(dr)*math.cos(lat1),
+                             math.cos(dr)-math.sin(lat1)*math.sin(lat2))
+    return (math.degrees(lat2), wrap_valid_longitude(math.degrees(lon2)))
+
+
